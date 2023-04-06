ca61b81f5f1d96664ef7c77c98b49895c52d4dbc
luofeng <982338665@qq.com>
2023-04-06 13:23:53 +0800

    用户任务介绍 及 简单使用 & 业务侧只集成了发起流程

--- camunda-engine/src/main/java/com/forestlake/workflow/Application.java
+++ camunda-engine/src/main/java/com/forestlake/workflow/Application.java
@@ -1,13 +1,34 @@
 package com.forestlake.workflow;
 
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
-  public static void main(String... args) {
-    SpringApplication.run(Application.class, args);
-  }
+
+    @Autowired
+    private IdentityService identityService;
+    @Autowired
+    private RuntimeService runtimeService;
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @GetMapping("/start/{processKey}")
+    public void start(@PathVariable(value = "processKey") String processKey) {
+        identityService.setAuthenticatedUserId("xiaoming");
+        runtimeService.startProcessInstanceByKey(processKey);
+    }
+
 
 }
